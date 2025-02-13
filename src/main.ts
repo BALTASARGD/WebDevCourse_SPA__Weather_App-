fd6af36005065bf549893221e203b7002051161b
BALTASARGD <https://github.com/BALTASARGD>
2025-02-13 18:41:19 +0100

    Add function to search for city weather and handle form submission

--- src/main.ts
+++ src/main.ts
@@ -19,3 +19,36 @@ weatherContainer.classList.add("mt-5", "p-4", "border", "rounded-lg");
 // Añadir los elementos al body
 document.body.appendChild(weatherContainer);
 document.body.appendChild(errorContainer);
+
+// Funciones principales
+
+formEl?.addEventListener("submit", searchCityWeather);
+
+// Función para manejar el envío del formulario
+function searchCityWeather(event: Event) {
+  event.preventDefault();
+  const city = getCity(event);
+  const sanitizedCity = sanitizeCity(city as string);
+  const isValidCity = isCityValid(sanitizedCity);
+
+  if (!isValidCity) {
+    displayInvalidCityMessage(false);
+    return;
+  }
+
+  // Verificar si los datos están en caché y son recientes
+  const cityData = localStorage.getItem(sanitizedCity);
+  if (cityData) {
+    const { timestamp, data } = JSON.parse(cityData);
+    if (Date.now() - timestamp < CACHE_EXPIRATION) {
+      console.log("✅ Usando datos en caché");
+      displayWeatherData(data);
+      return;
+    }
+  }
+
+  // Si no hay datos recientes en caché, obtener nuevos
+  getCityWeatherData(sanitizedCity);
+  spanEl?.classList.add("hidden");
+  showLoader();
+}
